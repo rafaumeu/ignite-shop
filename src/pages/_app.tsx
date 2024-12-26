6ff6dd40348a9cc34cfb3071622a2ac801268342
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-26 17:17:06 -0300

    feat(styles): configure global styles and Stitches theme with color palette

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
 
-export default function App({ Component, pageProps }) {
+globalStyles()
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
