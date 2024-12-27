2a8fd75170cddb468ca202c8d04211c40fc796f2
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-26 21:15:56 -0300

    feat(home): implement initial product catalog layout and add font size tokens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,26 @@
-import { styled } from '../styles'
-
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  }
-})
+import Image from 'next/image'
+import { HomeContainer, Product } from '../styles/pages/home'
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+import camiseta4 from '../assets/camisetas/4.png'
 export default function Home() {
   return (
-  <Button>Enviar</Button>
+  <HomeContainer>
+    <Product>
+      <Image src={camiseta1} width={520} height={480} alt=''/>
+      <footer>
+        <strong>Camiseta x</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+    <Product>
+      <Image src={camiseta2} width={520} height={480} alt=''/>
+      <footer>
+        <strong>Camiseta y</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+  </HomeContainer>
   );
 }
