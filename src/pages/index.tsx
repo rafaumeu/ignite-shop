bc656bc0d3374c6ced425fc48c6a5c5c73cf96ec
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-29 20:58:27 -0300

    refactor(pages): update static generation and component exports
    - Switch from `getServerSideProps` to `getStaticProps` in index page
    - Remove unused local image imports
    - Fix Success page component export to default export
    - Optimize imports and remove unnecessary assets
    
    Improves page generation strategy and export consistency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import Image from 'next/image'
 import { HomeContainer, Product } from '../styles/pages/home'
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
-import camiseta4 from '../assets/camisetas/4.png'
-import { useKeenSlider } from 'keen-slider/react'
+
 import 'keen-slider/keen-slider.min.css'
-import { stripe } from '../lib/stripe'
-import { GetServerSideProps } from 'next'
+import { useKeenSlider } from 'keen-slider/react'
+import { GetStaticProps } from 'next'
 import Stripe from 'stripe'
+import { stripe } from '../lib/stripe'
 
 interface HomeProps {
   products: {
@@ -43,7 +40,7 @@ export default function Home({products}: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
