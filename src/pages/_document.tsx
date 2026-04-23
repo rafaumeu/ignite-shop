ead4210609c401a0143774e7f9bd80cf010012f9
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-04-23 01:26:02 -0300

    feat: add Counter.dev analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
         <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }}/>
+        <script defer src="https://cdn.counter.dev/script.js" data-id="22668af5-0bdf-4cae-937b-83f9594dc79e" data-utcoffset="-3"></script>
       </Head>
       <body>
         <Main></Main>
